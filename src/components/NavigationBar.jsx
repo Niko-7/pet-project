c79b91792977927748c64afd276ae4dc529c75b9
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-08-28 18:07:55 +0100

    added conditional rendering for NavigationBar links

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,8 +2,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../styles/images/petprojectlogo_clean_icon.png";
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const NavigationBar = () => {
+  const location = useLocation();
+
   return (
     <>
       <Navbar className="navigation-bar__navbar">
@@ -25,6 +29,14 @@ const NavigationBar = () => {
             >
               About
             </Nav.Link>
+            {location.pathname === "/" && (
+              <Nav.Link
+                href="#brands"
+                className="text-white navigation-bar__links"
+              >
+                Brands
+              </Nav.Link>
+            )}
           </Nav>
         </Container>
       </Navbar>
