cc2cbc5d85059a38bee66e6c587ab4b85fe302aa
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-08-28 16:59:41 +0100

    added the HomePageSlider component and images

---
+++ src/components/HomePageSlider.jsx
@@ -0,0 +1,59 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import imageOne from "../styles/images/home-page-slider-images/1.jpg";
+import imageTwo from "../styles/images/home-page-slider-images/1.jpg";
+import imageThree from "../styles/images/home-page-slider-images/1.jpg";
+import imageFour from "../styles/images/home-page-slider-images/1.jpg";
+import imageFive from "../styles/images/home-page-slider-images/1.jpg";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 0,
+  },
+};
+
+const images = [imageOne, imageTwo, imageThree, imageFour, imageFive];
+
+const HomePageSlider = () => {
+  return (
+    <Carousel
+      pauseOnHover={true}
+      responsive={responsive}
+      ssr={true}
+      infinite={true}
+      autoPlay={true}
+      keyBoardControl={true}
+      containerClass="carousel-container"
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+      //   swipeable={true}
+      //   draggable={true}
+      //   showDots={true}
+      //   removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
+    >
+      <div>
+        {images.map((image, index) => {
+          return (
+            <img
+              className="carousel_image_center"
+              src={image}
+              alt="product"
+              key={`carousel-image-${index}`}
+            />
+          );
+        })}
+      </div>
+    </Carousel>
+  );
+};
+
+export default HomePageSlider;
