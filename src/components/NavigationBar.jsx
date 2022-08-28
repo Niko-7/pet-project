7d4196331041965ef90f751f871da2b1b07252bf
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-08-28 18:56:36 +0100

    added css

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,14 +3,11 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../styles/images/petprojectlogo_clean_icon.png";
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 const NavigationBar = () => {
-  const location = useLocation();
-
   return (
     <>
-      <Navbar className="navigation-bar__navbar">
+      <Navbar className="navigation-bar__navbar" sticky="top">
         <Container>
           <Navbar.Brand href="/">
             <img
@@ -29,14 +26,6 @@ const NavigationBar = () => {
             >
               About
             </Nav.Link>
-            {location.pathname === "/" && (
-              <Nav.Link
-                href="#brands"
-                className="text-white navigation-bar__links"
-              >
-                Brands
-              </Nav.Link>
-            )}
           </Nav>
         </Container>
       </Navbar>
