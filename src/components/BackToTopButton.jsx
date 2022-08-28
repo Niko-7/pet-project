50cddde8b8c075a27546e384e839019396aa69f9
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-08-28 18:55:48 +0100

    added the BackToTopButton component

---
+++ src/components/BackToTopButton.jsx
@@ -0,0 +1,13 @@
+const BackToTopButton = () => {
+  return (
+    <>
+      <div className="home-page-nav__container back-to-top-button__container">
+        <a href="#root" className="home-page-nav__link">
+          <button className="back-to-top-button__button">Back to top</button>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default BackToTopButton;
