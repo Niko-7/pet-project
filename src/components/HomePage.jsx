44a2de9e0d602a54b545e46d93a1c15dea2c32f2
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-08-28 16:58:58 +0100

    added the HomePage component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import HomePageSlider from "./HomePageSlider";
+
+const HomePage = () => {
+  return (
+    <div className="home-page__body">
+      <HomePageSlider />
+    </div>
+  );
+};
+
+export default HomePage;
