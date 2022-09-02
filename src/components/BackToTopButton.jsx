1a5048fd20adb293a72f80851a6c3fdc86468a1b
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-09-02 19:37:19 +0100

    updated the BackToTopButton

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
+
 const BackToTopButton = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div className="home-page-nav__container back-to-top-button__container">
-        <a href="#root" className="home-page-nav__link">
-          <button className="back-to-top-button__button">Back to top</button>
-        </a>
-      </div>
+      {scrollPosition > 300 && (
+        <div className="home-page-nav__container back-to-top-button__container">
+          <a href="#root" className="home-page-nav__link">
+            <button className="back-to-top-button__button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="currentColor"
+                className="bi bi-chevron-compact-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"
+                />
+              </svg>
+            </button>
+          </a>
+        </div>
+      )}
     </>
   );
 };
