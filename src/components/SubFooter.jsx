ddde945c32c066926bdd8c79b230354b9cc33d22
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-09-03 15:00:54 +0100

    added the SubFooter component

---
+++ src/components/SubFooter.jsx
@@ -0,0 +1,16 @@
+const SubFooter = () => {
+  return (
+    <>
+      <div className="sub-footer__body">
+        <a href="https://google.com" className="sub-footer__link">
+          Terms & Conditions
+        </a>
+        <a href="https://google.com" className="sub-footer__link">
+          Privacy Policy
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default SubFooter;
