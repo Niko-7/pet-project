0d0a6ca7c25e0e2e8190b153af171ed885ed3a49
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-09-05 20:44:50 +0100

    added the GoogleReviews component

---
+++ src/components/GoogleReviews.jsx
@@ -0,0 +1,12 @@
+const GoogleReviews = () => {
+  return (
+    <div id="google">
+      <h2 className="brand-images__title">
+        Δείτε τι λένε οι πελάτες μας για εμάς
+      </h2>
+      <div className="elfsight-app-6a054528-ba5e-4055-8eaa-7c7e1c3c85f2 google-reviews"></div>
+    </div>
+  );
+};
+
+export default GoogleReviews;
