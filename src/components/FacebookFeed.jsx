148fb12212cc3fd34899768ef73ea36eda402b70
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2022-09-05 20:44:27 +0100

    added the FacebookFeed component

---
+++ src/components/FacebookFeed.jsx
@@ -0,0 +1,12 @@
+const FacebookFeed = () => {
+  return (
+    <div id="facebook">
+      <h2 className="brand-images__title">
+        Τελευταίες αναρτήσεις στο Facebook
+      </h2>
+      <div className="powr-facebook-feed " id="bb20692a_1662403651"></div>
+    </div>
+  );
+};
+
+export default FacebookFeed;
